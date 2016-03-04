42e0b3cdc2410fdf23e8dd6fff39a67f15dc87f6
Hiromu Ishikawa <rock.lee.012086@gmail.com>
2016-03-05 00:53:45 +0900

    create new method by using pandas

--- batchgeocode.py
+++ batchgeocode.py
@@ -2,40 +2,29 @@
 # -*- coding: utf-8 -*-
 import sys
 import csv
+import pandas as pd
 #import codecs
 from pygeocoder import Geocoder, GeocoderResult
 
 def geocode(src,result):
 	
-	rfile = open(src,"r")
-	wfile = open(result,"w")
-	writer = csv.writer(wfile)
-	reader = csv.reader(rfile)
-	header = next(reader)
-	
-	header.append("lat")	
-	header.append("lng")
-
-	writer.writerow(header)
+	rfile = pd.read_csv(src, skiprows=1, names=("name", "address", "lat", "lng"))
+	print(rfile)
 
 	geocoder = Geocoder()
 
-	rows = []
-	for data in reader:
-		print("address:" + data[1] + " ...geocoding now")
-		geo = geocoder.geocode(data[1], language="ja")
+#	rows = []
+	for i in range(len(rfile)):
+		print("address:" + rfile.ix[i,"address"] + " ...geocoding now")
+		geo = geocoder.geocode(rfile.ix[i,"address"], language="ja")
 		if geo is not None :
 			print("success... (lat,lng)=",end="")	
-			row = [data[0],data[1],geo.coordinates[0],geo.coordinates[1]]
-			rows.append(row)
+			rfile.ix[i,["lat","lng"]] = [geo.coordinates[0],geo.coordinates[1]]
 			print(geo.coordinates)
 		else:
 			print("error...")
 
-	writer.writerows(rows)
-	wfile.close()
-	rfile.close()
-
+	rfile.to_csv("test2.csv", index=False)
 if __name__ == "__main__":
 	param = sys.argv
 	geocode(param[1],param[2])
